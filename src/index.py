59633060e85e7abc9a0191919f70557671b821bc
James Stratford <36135454+jcts3@users.noreply.github.com>
2018-12-29 13:09:34 +0000

    Improved panda dataframe creation -> faster as an array at once rather than piecemeal

--- src/index.py
+++ src/index.py
@@ -5,7 +5,7 @@ import datetime
 import csv
 
 print('starting soup')
-with open("messages/jamesstratford_koy9e4bcga.1/message.html") as fp:
+with open("../messages/jamesstratford_koy9e4bcga.1/message.html") as fp:
     soup = BeautifulSoup(fp, 'lxml')
 print('soup made')
 
@@ -18,10 +18,10 @@ def createPandaDF():
     print('creating pandas')
     messages = soup.body.find_all("div", class_="pam _3-95 _2pi0 _2lej uiBoxWhite noborder")
     print(len(messages))
-    # print(messages[1:100])
-    df = pd.DataFrame(columns=columns)
+    df2 = pd.DataFrame(columns=columns)
+    index=0
+    dataList = []
     for message in messages:
-        # print(message)
         dateTid = message.find("div", class_="_3-94 _2lem").get_text().split(' ')
         date = convertDate(dateTid)
         msg = message.find("div", class_="_3-96 _2let")
@@ -31,18 +31,32 @@ def createPandaDF():
         except:
             print('no text')
             text = '***NO TEXT***'
+        obj = [index, date, dateTid[3], person, text]
         csvfile.writerow(obj)
+        diction = {
+            'Index': index,
+            'Date': date,
+            'Time': dateTid[3],
+            'Person': person,
+            'Message': text
+        }
+#        tempdf = pd.DataFrame([obj], columns=columns)
+#        df = pd.concat(df, tempdf)
+        index += 1
+        dataList.append(diction)
+    df2 = pd.DataFrame(dataList, columns=columns)
+    return df2
         
 def convertDate(dateTid):
-    datum = dateTid[0]
-    monad = datetime.datetime.strptime(dateTid[1], '%b').month
-    ar = dateTid[2][2:4]
-    dateText = ('%s/%s/%s' %(monad, datum, ar))
+    datum = int(dateTid[0])
+    monad = int(datetime.datetime.strptime(dateTid[1], '%b').month)
+    ar = int(dateTid[2][2:4])
+    dateText = ('%i/%i/%i' %(monad, datum, ar))
     date = datetime.datetime.strptime(dateText, '%x').date()
     return date
 
-createPandaDF()
-
+#df2 = createPandaDF()
+print('properly done')
 # for message in messages
 
 
